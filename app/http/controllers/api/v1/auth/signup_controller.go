54d3540e11c7b83ed816456c496ff55b411af768
cwan <cwan@iiiview.com>
2022-04-04 00:18:28 +0800

    feat:json解析和验证封装

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -2,12 +2,10 @@
 package auth
 
 import (
-    "fmt"
-    "net/http"
-
     v1 "gohub/app/http/controllers/api/v1"
     "gohub/app/models/user"
     "gohub/app/requests"
+    "net/http"
 
     "github.com/gin-gonic/gin"
 )
@@ -19,35 +17,25 @@ type SignupController struct {
 
 // IsPhoneExist 检测手机号是否被注册
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
-
-    // 初始化请求对象
+    // 获取请求参数，并做表单验证
     request := requests.SignupPhoneExistRequest{}
-
-    // 解析 JSON 请求
-    if err := c.ShouldBindJSON(&request); err != nil {
-        // 解析失败，返回 422 状态码和错误信息
-        c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-            "error": err.Error(),
-        })
-        // 打印错误信息
-        fmt.Println(err.Error())
-        // 出错了，中断请求
-        return
-    }
-
-    // 表单验证
-    errs := requests.ValidateSignupPhoneExist(&request, c)
-    // errs 返回长度等于零即通过，大于 0 即有错误发生
-    if len(errs) > 0 {
-        // 验证失败，返回 422 状态码和错误信息
-        c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-            "errors": errs,
-        })
+    if ok := requests.Validate(c, &request, requests.SignupPhoneExist); !ok {
         return
     }
 
     //  检查数据库并返回响应
     c.JSON(http.StatusOK, gin.H{
         "exist": user.IsPhoneExist(request.Phone),
+    })
+}
+
+// IsEmailExist 检测邮箱是否已注册
+func (sc *SignupController) IsEmailExist(c *gin.Context) {
+    request := requests.SignupEmailExistRequest{}
+    if ok := requests.Validate(c, &request, requests.SignupEmailExist); !ok {
+        return
+    }
+    c.JSON(http.StatusOK, gin.H{
+        "exist": user.IsEmailExist(request.Email),
     })
 }
