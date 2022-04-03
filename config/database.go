3a103691be0876646831902b5b57e2cc7bd2d9be
cwan <cwan@iiiview.com>
2022-04-04 00:01:52 +0800

    feat:手机是否注册接口

--- config/database.go
+++ config/database.go
@@ -18,7 +18,7 @@ func init() {
                 "host":     config.Env("DB_HOST", "127.0.0.1"),
                 "port":     config.Env("DB_PORT", "3306"),
                 "database": config.Env("DB_DATABASE", "gohub"),
-                "username": config.Env("DB_USERNAME", ""),
+                "username": config.Env("DB_USERNAME", "root"),
                 "password": config.Env("DB_PASSWORD", ""),
                 "charset":  "utf8mb4",
 
