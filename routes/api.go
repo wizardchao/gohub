d98b06de6ee5b179c601919cfaab00767d7d73d9
cwan <cwan@iiiview.com>
2022-04-09 22:40:08 +0800

    发送短信验证码

--- routes/api.go
+++ routes/api.go
@@ -2,29 +2,32 @@
 package routes
 
 import (
-    "gohub/app/http/controllers/api/v1/auth"
+	"gohub/app/http/controllers/api/v1/auth"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 // RegisterAPIRoutes 注册网页相关路由
 func RegisterAPIRoutes(r *gin.Engine) {
 
-    // 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
-    v1 := r.Group("/v1")
-    {
-        authGroup := v1.Group("/auth")
-        {
-            suc := new(auth.SignupController)
-            // 判断手机是否已注册
-            authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
-            // 判断 Email 是否已注册
-            authGroup.POST("/signup/email/exist", suc.IsEmailExist)
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	v1 := r.Group("/v1")
+	{
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+			// 判断 Email 是否已注册
+			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
 
-             // 发送验证码
-           vcc := new(auth.VerifyCodeController)
-              // 图片验证码，需要加限流
-           authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
-        }
-    }
+			// 发送验证码
+			vcc := new(auth.VerifyCodeController)
+			// 图片验证码，需要加限流
+			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
+
+			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
+			authGroup.POST("/verify-codes/phone", vcc.SendUsingPhone)
+		}
+	}
 }
