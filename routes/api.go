d3e7594e224be6c77d4364e841d4a19d6b142de6
cwan <cwan@iiiview.com>
2022-04-24 20:01:21 +0800

    feat:当前用户接口

--- routes/api.go
+++ routes/api.go
@@ -50,11 +50,12 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/verify-codes/email", middlewares.LimitPerRoute("20-H"), vcc.SendUsingEmail)
 			// 图片验证码
 			authGroup.POST("/verify-codes/captcha", middlewares.LimitPerRoute("50-H"), vcc.ShowCaptcha)
-
-			uc := new(controllers.UsersController)
-
-			// 获取当前用户
-			v1.GET("/user", middlewares.AuthJWT(), uc.CurrentUser)
 		}
+
+		uc := new(controllers.UsersController)
+
+		// 获取当前用户
+		v1.GET("/user", middlewares.AuthJWT(), uc.CurrentUser)
 	}
+
 }
